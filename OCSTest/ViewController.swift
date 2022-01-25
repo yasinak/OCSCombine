f08b6c1960d50e9e296186f2c4db8d5158aa9ab3
Yasin AKINCI <yakinci@hubside.com>
2022-01-25 20:31:42 +0100

    Initial Commit

---
+++ OCSTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCSTest
+//
+//  Created by Yasin AKINCI on 25/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
