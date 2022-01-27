823a5ab78c8d473bd031788ed0316566ad840ac7
Yasin AKINCI <yakinci@hubside.com>
2022-01-27 01:50:43 +0100

    Add 2 Models tests

--- OCSTestTests/OCSTestTests.swift
+++ OCSTestTests/OCSTestTests.swift
@@ -6,12 +6,16 @@
 //
 
 import XCTest
+import Combine
 @testable import OCSTest
 
 class OCSTestTests: XCTestCase {
 
+    var cancellables: Set<AnyCancellable>!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        cancellables = []
     }
 
     override func tearDownWithError() throws {
@@ -24,8 +28,45 @@ class OCSTestTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+//        let moviesViewModel = MoviesViewModel()
+//        moviesViewModel.searchQuery = "value"
+//        moviesViewModel.$searchQuery.sink { value in
+//
+//        }.store(in: &cancellables)
+        
+//        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func testMoviesSearchModel() {
+        
+        guard let path = Bundle(for: type(of: self)).path(forResource: "MoviesSearch", ofType: "json") else {
+            fatalError("MoviesSearch.json not found") }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            let response = try JSONDecoder().decode(Movies.self, from: data)
+
+            XCTAssertNotNil(response.title, "Recherche")
+            XCTAssertNotEqual(response.contents?.count, 0)
+        } catch {
+            fatalError("CurrentWeather.json file format not correct")
+        }
     }
 
+    func testMovieDetailsModel() {
+        
+        guard let path = Bundle(for: type(of: self)).path(forResource: "MovieDetails", ofType: "json") else {
+            fatalError("MovieDetails.json not found") }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            let response = try JSONDecoder().decode(MovieDetails.self, from: data)
+
+            XCTAssertNotNil(response.template, "detail")
+            XCTAssertEqual(response.contents?.title?[0].value, "GAME CHANGE")
+        } catch {
+            fatalError("MovieDetails.json file format not correct")
+        }
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
